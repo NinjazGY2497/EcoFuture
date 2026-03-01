12a03f95208885b3d32b955f19593bec8c48802f
NinjazGY2497 <gyagau@gmail.com>
2026-02-28 23:10:36 -0500

    Before Aarush's chart.js changes

---
+++ frontEnd/js/requestBackend.js
@@ -0,0 +1,31 @@
+async function requestBackend(location, animal, timeframe) {
+    const BACKEND_URL = "http://127.0.0.1:2497/ai-response";
+
+    const response = await fetch(BACKEND_URL, {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+            location: location,
+            animal: animal,
+            timeframe: timeframe,
+        })
+    });
+
+    if (!response.ok) throw new Error("Network response was not ok");
+    return await response.json();
+}
+
+// Trigger "Changes you made may not be saved" prompt to prevent lots of refreshing
+window.addEventListener('beforeunload', (event) => {
+    event.preventDefault();
+    event.returnValue = ''; 
+});
+
+// Requesting
+const urlData = new URLSearchParams(window.location.search);
+
+if (!urlData.has('location') && !urlData.has('animal') && !urlData.has('timeframe')) {
+    window.location.href = 'index.html'; // Redirect back to main page
+}
+const data = requestBackend(urlData.get('location'), urlData.get('animal'), urlData.get('timeframe'))
+console.log(data)
